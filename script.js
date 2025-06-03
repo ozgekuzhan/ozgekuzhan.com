f1d6c33fff891ec8a51efa1b1bed4764e5b988dd
ozgekuzhan <ozgekuzhan@gmail.com>
2025-06-03 02:55:19 +0200

    other infos added

--- script.js
+++ script.js
@@ -52,4 +52,20 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 
   document.getElementById("current-year").textContent = new Date().getFullYear();
+
+
+  document.querySelectorAll('nav a').forEach(anchor => {
+      anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        const targetId = this.getAttribute('href');
+        const targetElement = document.querySelector(targetId);
+
+        if (targetElement) {
+          targetElement.scrollIntoView({
+            behavior: 'smooth'
+          });
+        }
+      });
+  });
 });
