772fb24ceb98916a723c91872ed31b75ab2a812a
ozgekuzhan <ozgekuzhan@gmail.com>
2025-06-18 23:31:48 +0200

    scroll to top added

--- script.js
+++ script.js
@@ -99,3 +99,21 @@ document.addEventListener("DOMContentLoaded", function () {
 
   createStarfield();
 });
+document.addEventListener("DOMContentLoaded", function () {
+  const btn = document.getElementById("scrollToTopBtn")
+
+  window.onscroll = function () {
+    if (document.body.scrollTop > 300 || document.documentElement.scrollTop > 300) {
+      btn.style.display = "block"
+    } else {
+      btn.style.display = "none"
+    }
+  }
+
+  btn.addEventListener("click", function () {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
+  })
+})
